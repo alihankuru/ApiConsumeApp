21ff761f6429cd24f96c32c94cee512fd1c3b632
AK <ali.kuru6@hotmail.com>
2024-01-16 17:02:12 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/TestimonialManager.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class TestimonialManager:ITestimonialService
+    {
+        private readonly ITestimonialDal _TestimonialDal;
+
+        public TestimonialManager(ITestimonialDal testimonialDal)
+        {
+            _TestimonialDal = testimonialDal;
+        }
+
+        public void TDelete(Testimonial t)
+        {
+            _TestimonialDal.Delete(t);
+        }
+
+        public Testimonial TGetByID(int id)
+        {
+           return _TestimonialDal.GetByID(id);
+        }
+
+        public List<Testimonial> TGetList()
+        {
+            return _TestimonialDal.GetList();
+        }
+
+        public void TInsert(Testimonial t)
+        {
+            _TestimonialDal.Insert(t);
+        }
+
+        public void TUpdate(Testimonial t)
+        {
+            _TestimonialDal.Update(t);
+        }
+    }
+}
